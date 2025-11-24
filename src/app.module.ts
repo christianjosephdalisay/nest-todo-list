a5b943422267a428d30fe25a52e2fdcdfb657142
Christian Joseph Dalisay <7392803+launchwithchris@users.noreply.github.com>
2025-11-24 18:30:51 +0800

    chore(mongoose): install Nest Mongoose package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './todo/todo.controller';
 import { DocsController } from './docs/docs.controller';
+import { AdminController } from './admin/admin.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TodoController, DocsController],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  controllers: [AppController, TodoController, DocsController, AdminController],
   providers: [AppService],
 })
 export class AppModule {}
