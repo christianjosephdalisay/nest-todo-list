078efb9a3a4466b431c83704c1df58bed7e71ebd
Christian Joseph Dalisay <7392803+launchwithchris@users.noreply.github.com>
2025-11-24 18:32:21 +0800

    Revert "chore(mongoose): install Nest Mongoose package"
    This reverts commit a5b943422267a428d30fe25a52e2fdcdfb657142.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './todo/todo.controller';
 import { DocsController } from './docs/docs.controller';
-import { AdminController } from './admin/admin.controller';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
-  controllers: [AppController, TodoController, DocsController, AdminController],
+  imports: [],
+  controllers: [AppController, TodoController, DocsController],
   providers: [AppService],
 })
 export class AppModule {}
